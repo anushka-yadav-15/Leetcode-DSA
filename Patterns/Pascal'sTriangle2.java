62c14e785200ba1e4cfbd30b293ad842cfbfec2a
Anushka Yadav <anushka-yadav-15@gmail.com>
2026-01-03 14:36:59 +0530

    addcodes

---
+++ Patterns/Pascal'sTriangle2.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i = 0; i <= rowIndex ; i++){
+            List<Integer> row = new ArrayList<>();
+
+            for(int j =0 ; j <= i; j++){
+                if(j == 0 || j == i){
+                    row.add(1);
+                }
+                else{
+                    int value = result.get(i-1).get(j-1) + result.get(i-1).get(j);
+                    row.add(value);
+                }
+            }
+            result.add(row);
+        }
+        return result.get(rowIndex);
+        
+    }
+}
